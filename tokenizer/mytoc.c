9ffc811d10279f30772ca44eda1a2693d36d5a50
JoseACabreraM <jacabreramaynez@miners.utep.edu>
2017-09-05 12:29:26 -0600

    Added comments

--- tokenizer/mytoc.c
+++ tokenizer/mytoc.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Takes a char pointer and changes it based on user input
+// Used to change the tokenizer delimiter character
 void changeDelimiter(char* delim){
     printf("\nInput a new delimiter character: ");
     scanf("%[^\n]%*c", delim);
     write(1,"\n", 1);
 }
 
+// Compares two strings, returns 1 if they're equal, 0 otherwise
 int stringCompare(char* fString, char* sString){
 
     int i = 0;
@@ -20,24 +23,31 @@ int stringCompare(char* fString, char* sString){
     return(fString[i] == '\0' && sString[i] == '\0');
 }
 
+// Given a string, counts the number of words
+// Words are considered as sequence of characters, separated by the input delimiter
 int numberOfWords(char* inputString, char delim){
 
+    // Directly return zero if the first character is a newspace
+    // To avoid doing unnecessary work if user just pressed enter
     if (*inputString == '\n'){
         return 0;
     }
 
-    char* temp = inputString;
-    int i = 0;
+    char* temp = inputString; // To traverse the input string
+    int i = 0; // Keep track of number of words
     int wordFound = 0;
 
     while(1){
+        // Considers a word has been found if the current character is not currenlty a delimiter or newspace
         if (*temp != delim && *temp != '\n'){
             wordFound = 1;
         }
+        // If a word has been found and a delimiter or newspace is reached, consider that word as accounted for
         if ((wordFound && *temp == delim) || (wordFound && *temp == '\n')){
             i++;
-            wordFound = 0;
+            wordFound = 0; // Not considering word until a non delimiter char is found
         }
+        // Reached end of input string, return the word counter
         if (*temp == '\0'){
             return i;
         }
@@ -47,6 +57,9 @@ int numberOfWords(char* inputString, char delim){
     return i;
 }
 
+// Receives a string and a delimiter character, returns an pointer vector
+// which contains pointers to freshly allocated copies of the individual words
+// within the input string. Words are considered as sequence of characters, separated by the input delimiter
 char** myToc(char* inputString, char delim){
 
     int numWords = numberOfWords(inputString, delim);
@@ -57,25 +70,31 @@ char** myToc(char* inputString, char delim){
     char* wordStart = inputString;
 
     while(1){
+
+        // If currently not in a delimiter, newspace, or end of string, increment current word length counter
         if(*temp != delim && *temp != '\n' && *temp != '\0'){
             currentStringLength++;
+            // If at the start of a word, save the pointer for copying when lenght is calculated
             if (readingWord){
                 readingWord = 0;
                 wordStart = temp;
             }
         }
-        if((*temp == delim || *temp == '\0') && !readingWord){
-            tokenizedStrings[i] = (char*) malloc(sizeof(char) * (currentStringLength + 1));
 
+        // If a delimiter or end of string has been reached and it's not the start of a new word
+        if((*temp == delim || *temp == '\0') && !readingWord){
+            tokenizedStrings[i] = (char*) malloc(sizeof(char) * (currentStringLength + 1)); // Allocate the necessary space for the new word
+            // Using the pointer at the start of the word, copy its contents into the vector
             for (j = 0; j < currentStringLength; j++){
                 tokenizedStrings[i][j] = *wordStart;
                 wordStart++;
             }
-            tokenizedStrings[i][currentStringLength] = '\0';
-            currentStringLength = 0;
+            tokenizedStrings[i][currentStringLength] = '\0'; // Append the end of string char
+            currentStringLength = 0; // Reset word length counter for next word
             i++;
-            readingWord = 1;
+            readingWord = 1; // Assume a new word is incoming
 
+            // When the input string has been exhausted, break out of the loop
             if(*temp == '\0'){
                 break;
             }
@@ -84,11 +103,12 @@ char** myToc(char* inputString, char delim){
         temp++;
     }
 
-    tokenizedStrings[numWords] = '\0';
+    tokenizedStrings[numWords] = '\0'; // Append end of string character to the token vector
 
-    return tokenizedStrings;
+    return tokenizedStrings; // Return token vector
 }
 
+// Alternative tokenizer implementation that doesn't copy words, it modifies the input string
 char** myToc2(char* inputString, char delim){
 
     int numWords = numberOfWords(inputString, delim);
@@ -98,15 +118,18 @@ char** myToc2(char* inputString, char delim){
     char* temp = inputString;
 
     while(1){
+        // When original end of string is reached, copy last word address and return token vector
         if (*temp == '\0'){
             tokenizedStrings[i] = temp;
             return tokenizedStrings;
         }
+        // If at the start of a new word, not counting the delimiter, copy the current address into the token vecotr
         if (insertWord && *temp != delim){
             tokenizedStrings[i] = temp;
             i++;
-            insertWord = 0;
+            insertWord = 0; // No longer at the start of a word
         }
+        // If a delimiter or newspace character is reached, replace it by end of string character
         if (*temp == delim || *temp == '\n'){
             insertWord = 1;
             *temp = '\0';
--- tokenizer/test.c
+++ tokenizer/test.c
@@ -5,7 +5,7 @@
 
 int main(){
 
-    int len = 1024;
+    int len = 1024, tocChoice = 1;
     char inputString[len], delim = ' ';
     char** tokenizedString;
 
@@ -14,22 +14,51 @@ int main(){
     write(1,"$ ", 2);
     fgets (inputString, len, stdin) ;
 
+    // To end execution
     if (stringCompare(inputString, "exit\n")){
+        printf("\nEnd of Execution\n\n");
         return 0;
     }
+
+    // To change the delimiter character
     if (stringCompare(inputString, "nDel\n")){
         changeDelimiter(&delim);
         goto LOOP;
     }
+
+    // To restore the delimiter character
     if (stringCompare(inputString, "rDel\n")){
         delim = ' ';
+        printf("\nRestored default delimiter\n\n");
+        goto LOOP;
+    }
+
+    // To change the tokenizer function to the alternative one
+    if (stringCompare(inputString, "aToc\n")){
+        tocChoice = 0;
+        printf("\nUsing alternative tokenizer\n\n");
+        goto LOOP;
+    }
+
+    // To restore the original tokenizer function
+    if (stringCompare(inputString, "rToc\n")){
+        tocChoice = 1;
+        printf("\nRestored regular tokenizer\n\n");
         goto LOOP;
     }
 
     int numWords = numberOfWords(inputString, delim);
-    tokenizedString = myToc(inputString, delim);
+
+    // Create the token vector depending on the tokenizer function
+    if (tocChoice){
+        tokenizedString = myToc(inputString, delim);
+    } else {
+        tokenizedString = myToc2(inputString, delim);
+    }
+
     int i;
 
+    // Prints the token vector, token by toke, character by character
     write(1,"\n", 1);
     for (i = 0; i < numWords; i++){
         char* temp = tokenizedString[i];
@@ -41,10 +70,14 @@ int main(){
     }
     write(1,"\n", 1);
 
-    for (i = 0; i < numWords + 1; i++){
-        free(tokenizedString[i]);
+    // Only free each token memory if they were allocated using the regular tokenizer
+    if (tocChoice){
+        for (i = 0; i < numWords + 1; i++){
+            free(tokenizedString[i]);
+        }
     }
 
+    // Free the pointer array from memory
     free(tokenizedString);
     goto LOOP;
 
